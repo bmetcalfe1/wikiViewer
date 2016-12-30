3552c80c273b21a256d5e12cdaa5f4d106297b53
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-12-30 18:20:44 -0500

    before branching

--- wikiViewer.js
+++ wikiViewer.js
@@ -72,7 +72,6 @@ $(document).ready(function() {
         type: "GET",
         url: api + userSearch + cb,
         contentType: "application/json; charset=utf-8",
-        async: false,
         dataType: "jsonp",
         success: function (data) {
           var html = "";
@@ -122,20 +121,19 @@ $(document).ready(function() {
     var underscoreArticle = clickedArticle.replace(/ /g,"_");
 
     $.ajax({
-        type: "GET",
-        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&page=" + underscoreArticle +  "&callback=?",
-        contentType: "application/json; charset=utf-8",
-        async: false,
-        dataType: "json",
-        success: function (data, textStatus, jqXHR) {
+      type: "GET",
+      url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&page=" + underscoreArticle +  "&callback=?",
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function (data, textStatus, jqXHR) {
 
-            var markup = data.parse.text["*"];
-            var blurb = $('<div></div>').html(markup);
-            $('.article-box').html($(blurb).find('p'));
+          var markup = data.parse.text["*"];
+          var blurb = $('<div></div>').html(markup);
+          $('.article-box').html($(blurb).find('p'));
 
-        },
-        error: function (errorMessage) {
-        }
+      },
+      error: function (errorMessage) {
+      }
     });
 
   }
